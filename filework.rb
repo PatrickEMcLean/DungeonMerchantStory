148f78fd07706b25b15ec858d93a9316cb3ef4a1
PatrickEMcLean <patrickemclean@gmail.com>
2013-02-09 18:33:18 -0500

    words in draft=  5012

--- filework.rb
+++ filework.rb
@@ -1,13 +1,15 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 
 wordcount=`wc 01-Draft.md`
 wordcount=wordcount.split(" ")
-exec "git add -A"
-exec "git commit -m #{"words in draft= " + wordcount[1]}"
-exec "git push origin master"
+system("git add -A")
+system("git commit -m \"words in draft= \ #{wordcount[1]}\"")
+system("git push origin master")
 
 # ===========================================================
+#
+#
 # Warning, don't change the last line to something like this
 
 # 'git push origin master -u username -p password'
